21d72f4b831ea29041242ea4791338832402f24b
gtc9d8jcry-ai <gtc9d8jcry@privaterelay.appleid.com>
2026-06-26 21:02:58 +0100

    Create routeTree.gen.ts
    Add src/routeTree.gen.ts - basic router setup

---
+++ routeTree.gen.ts
@@ -0,0 +1,7 @@
+import { createRoute } from '@tanstack/react-router'
+
+export const routeTree = createRoute({
+  // We'll expand this as we add more routes
+}).addChildren([
+  // Add routes here as we migrate them
+])
